92b09004899734c239043571f27008c674704136
Michael Goodness <mgoodness@widen.com>
2016-05-10 10:01:53 -0500

    Switched from oh-my-fish to fisherman

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,7 @@ alias gd 'git diff'
 alias gfa 'git fetch --all --prune'
 alias gl 'git pull'
 alias glog 'git log --color --decorate --graph --oneline'
+alias glstat 'git log --color --decorate --stat'
 alias gm 'git merge'
 alias gp 'git push'
 alias gr 'git remote'
@@ -26,6 +27,7 @@ alias grmv 'git remote rename'
 alias grrm 'git remote remove'
 alias grset 'git remote set-url'
 alias grup 'git remote update'
+alias gshow 'git show --decorate'
 alias gst 'git status --short --branch'
 alias gsta 'git stash'
 alias gstp 'git stash pop'
@@ -33,18 +35,18 @@ alias gstp 'git stash pop'
 # Misc aliases
 alias curl-trace 'curl -w "@$HOME/.dotfiles/curl-format" -o /dev/null -s'
 alias dockerclean 'docker rm (docker ps -a -q); docker rmi (docker images -q --filter "dangling=true")'
-alias dockergo 'docker-machine restart; and eval (docker-machine env default)'
 alias fixbt 'sudo killall coreaudiod'
 alias flushdns 'sudo killall -HUP mDNSResponder'
 alias grep 'ack -i'
 alias kubesys 'kubectl --namespace=kube-system'
 alias which 'type -a'
 
+# Terraform aliases
+alias tfgp 'terraform get --update; and terraform plan'
+
 # Set variables
 set -gx EDITOR "vim"
 set -gx GOPATH "$HOME/dev/golang"
 set -gx GRADLE_OPTS "-Xmx512m"
 set -gx JAVA_HOME (/usr/libexec/java_home -v "1.8*")
-set -gx PATH $PATH "$GOPATH/bin" "$HOME/william/bin"
-
-set_secure
+set -gx PATH $PATH "$GOPATH/bin" "$HOME/dev/william/bin"
--- fish/fish_right_prompt.fish
+++ fish/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-  set -l right_prompt (set_color purple)
+  set -l right_prompt (set_color magenta)
 
   if test -n "$AWS_DEFAULT_PROFILE"
     echo -e -n -s $right_prompt "[$AWS_DEFAULT_PROFILE]"
