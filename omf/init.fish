382de9ac5b0b6a6209fd8c5f2854400999221f6e
Michael Goodness <mgoodness@widen.com>
2017-02-08 12:02:53 -0600

    updated git aliases

--- omf/init.fish
+++ omf/init.fish
@@ -1,28 +1,19 @@
 # Git aliases
-alias ga 'git add'
 alias gaa 'git add --all'
-alias gb 'git branch'
-alias gba 'git branch -a'
+alias gba 'git branch --all'
 alias gbr 'git branch --remote'
-alias gc 'git commit -v'
-alias gc! 'git commit --amend -v'
-alias gca 'git commit -a -v'
-alias gca! 'git commit -a --amend -v'
-alias gcm 'git checkout master'
-alias gco 'git checkout'
-alias gd 'git diff'
+alias gc 'git commit --gpg-sign --verbose'
+alias gc! 'git commit --amend --gpg-sign --verbose'
+alias gca 'git commit --all --gpg-sign --verbose'
+alias gca! 'git commit --all --gpg-sign --amend --verbose'
 alias gfa 'git fetch --all --prune'
-alias gl 'git pull'
 alias glog 'git log --color --decorate --graph --oneline'
 alias glstat 'git log --color --decorate --stat'
-alias gm 'git merge'
-alias gp 'git push'
 alias gpdo 'git push --delete origin'
-alias gpsu 'git push --set-upstream'
-alias gr 'git remote'
 alias grba 'git rebase --abort'
 alias grbc 'git rebase --continue'
-alias grbi 'git rebase -i'
+alias grbi 'git rebase --interactive'
+alias grum 'git rebase upstream/master'
 alias grh 'git reset HEAD'
 alias grhh 'git reset HEAD --hard'
 alias grmv 'git remote rename'
@@ -31,23 +22,27 @@ alias grset 'git remote set-url'
 alias grup 'git remote update'
 alias gshow 'git show --decorate'
 alias gst 'git status --short --branch'
-alias gsta 'git stash'
-alias gstp 'git stash pop'
 
 # Misc aliases
 alias brewski 'brew update; and brew upgrade; brew cleanup; brew cask cleanup; brew doctor'
 alias curl-trace 'curl -w "@$HOME/.dotfiles/curl-format" -o /dev/null -s'
-alias dockerclean 'docker rm (docker ps -a -q); docker rmi (docker images -q --filter "dangling=true")'
 alias fixbt 'sudo killall coreaudiod'
 alias flushdns 'sudo killall -HUP mDNSResponder'
 alias grep 'ack -i'
 
 # Kubernetes aliases
+alias kube-coll 'kubectl --namespace=collective'
+alias kube-dam 'kubectl --namespace=dam'
+alias kube-drain 'kubectl drain --force --ignore-daemonsets --delete-local-data'
 alias kube-edge 'kubectl --namespace=edge'
+alias kube-juice 'kubectl --namespace=polyjuice'
 alias kube-lens 'kubectl --namespace=lens'
+alias kube-mon 'kubectl --namespace=monitoring'
 alias kube-plat 'kubectl --namespace=platform'
+alias kube-print 'kubectl --namespace=printui-connector'
 alias kube-sys 'kubectl --namespace=kube-system'
 alias kube-wip 'kubectl --namespace=wip'
+alias kube-wolf 'kubectl --namespace=graywolf'
 
 # Terraform aliases
 alias tfgp 'terraform get --update; and terraform plan'
@@ -56,7 +51,11 @@ alias tfgp 'terraform get --update; and terraform plan'
 set -gx EDITOR "vim"
 set -gx GOPATH "$HOME/dev/golang"
 set -gx GRADLE_OPTS "-Xmx512m"
+set -gx HELM_HOME "$HOME/.helm"
 set -gx JAVA_HOME (/usr/libexec/java_home -v "1.8*")
-set -gx PATH $PATH "$GOPATH/bin" "$HOME/dev/william/bin"
+set -gx HOMEBREW_GITHUB_API_TOKEN "364252ea0a7d30c4c38ce6dd6599c9c321acc053"
+
+# GPG
+gpg-init
 
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
